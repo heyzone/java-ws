bde15464d875ad026f0ae51d3850a7fba98b9616
heyzone <harrywujy520@gmail.com>
2026-06-10 18:04:58 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -113,7 +113,7 @@ public class App {
         NEZHA_SERVER = getEnvValue(envFromFile, "NEZHA_SERVER", "");
         NEZHA_PORT = getEnvValue(envFromFile, "NEZHA_PORT", "");
         NEZHA_KEY = getEnvValue(envFromFile, "NEZHA_KEY", "");
-        DOMAIN = getEnvValue(envFromFile, "DOMAIN", "wisphwjy520.wispbyte.org");
+        DOMAIN = getEnvValue(envFromFile, "DOMAIN", "");
         SUB_PATH = getEnvValue(envFromFile, "SUB_PATH", "sub");
         NAME = getEnvValue(envFromFile, "NAME", "wisp0");
         
@@ -128,7 +128,7 @@ public class App {
         // 处理端口
         String portStr = getEnvValue(envFromFile, "SERVER_PORT", null);
         if (portStr == null) {
-            portStr = getEnvValue(envFromFile, "PORT", "13313");
+            portStr = getEnvValue(envFromFile, "PORT", "3000");
         }
         PORT = Integer.parseInt(portStr);
         
